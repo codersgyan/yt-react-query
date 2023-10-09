138adf653476603649df7ab4ffbb1111227c7f32
Rakesh K <codersgyan@gmail.com>
2023-10-09 19:58:42 +0300

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function App() {
     return (
         <>
-            <h1>React query</h1>
+            <h1>The Awesome React query</h1>
+            <div>
+                <Link to="/">Home</Link> <br />
+                <Link to="/products">Products</Link>
+            </div>
         </>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import Products from './Products.jsx';
+import Product from './Product.jsx';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+    },
+    {
+        path: '/products',
+        element: <Products />,
+    },
+    {
+        path: '/products/:productId',
+        element: <Product />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
